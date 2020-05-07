c3430fda164c073a5376860a28101177abe5d9e9
Raj Shekhar <channiecx954@gmail.com>
2020-05-08 00:27:48 +0530

    check day wiht ejs

--- app.js
+++ app.js
@@ -3,7 +3,17 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.set('view engine', 'ejs')
+var todaysDate = new Date()
+let day = ''
+
+if(todaysDate.getDay() === 6 || todaysDate.getDay() === 0 ) 
+    day = 'Weekend'
+else
+    day = 'Weekday'
+
 app.get('/', (req,res) => {
+    res.render('list', {dayOfWeek: day})
     res.sendFile(__dirname + '/index.html')
 })
 
