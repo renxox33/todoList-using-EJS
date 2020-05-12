9098e39d9fdc261c07e6c2a24b16dc376ef5bd27
Raj Shekhar <channiecx954@gmail.com>
2020-05-13 02:23:41 +0530

    Markup and ejs complete

--- app.js
+++ app.js
@@ -1,20 +1,41 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
-var todaysDate = new Date()
-let day = ''
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static('css'))
 
-if(todaysDate.getDay() === 6 || todaysDate.getDay() === 0 ) 
-    day = 'Weekend'
-else
-    day = 'Weekday'
+let today = new Date()
+let options = {
+    weekday: 'long',
+    day: 'numeric', 
+    month: 'long'
+}
+
+let todoItems = []
+
+let day = today.toLocaleString('en-US', options)
+
 
 app.get('/', (req,res) => {
-    res.render('list', {dayOfWeek: day})
-    res.sendFile(__dirname + '/index.html')
+
+    res.render('list', { dayOfWeek: day, newListItem: todoItems})
+})
+
+app.post('/', (req,res) => {
+
+    todoItems.push(req.body.newTodoItem)
+    console.log(todoItems);
+    
+    res.redirect('/')
+    
+})
+
+app.get('/about', (req,res) => {
+    res.render('about', { dayOfWeek: day })
 })
 
 app.listen(port, () => console.log('Listening to requests on port '+ port)
